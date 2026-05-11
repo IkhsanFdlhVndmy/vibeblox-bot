e886ddc166f40367428eedd0004f6f049406faf4
IkhsanFdlhVndmy <157859525+IkhsanFdlhVndmy@users.noreply.github.com>
2026-05-12 04:40:22 +0700

    Add isAnonymous field to User schema

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true },
-    uangMasuk: { type: Number, default: 0 }
+    uangMasuk: { type: Number, default: 0 },
+    isAnonymous: { type: Boolean, default: false } // <--- TAMBAHAN BARU
 });
 
 module.exports = mongoose.model('User', UserSchema);
