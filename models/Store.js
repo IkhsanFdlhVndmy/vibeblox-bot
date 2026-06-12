ebb9d30a77baccfd332fb634521aeb42d02c53ef
IkhsanFdlhVndmy <157859525+IkhsanFdlhVndmy@users.noreply.github.com>
2026-06-13 06:02:50 +0700

    Add menuMessages field to StoreSchema
    Added menuMessages field to StoreSchema for storing message details.

--- models/Store.js
+++ models/Store.js
@@ -4,8 +4,9 @@ const StoreSchema = new mongoose.Schema({
     storeId: { type: String, default: 'VIBEBLOX_FINANCE' },
     totalUangMasuk: { type: Number, default: 0 },
     totalUangKeluar: { type: Number, default: 0 },
-    leaderboardChannelId: { type: String, default: null }, // Buat nginget channel-nya
-    leaderboardMessageId: { type: String, default: null }  // Buat nginget pesan leaderboard-nya
+    leaderboardChannelId: { type: String, default: null },
+    leaderboardMessageId: { type: String, default: null },
+    menuMessages: { type: Array, default: [] } // <-- TAMBAHAN BARU: [{ channelId, messageId, type }]
 });
 
 module.exports = mongoose.model('Store', StoreSchema);
