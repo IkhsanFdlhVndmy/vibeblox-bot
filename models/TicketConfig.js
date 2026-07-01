1872826454e1ee51de8e4f776d0e0ccfbea613c4
IkhsanFdlhVndmy <157859525+IkhsanFdlhVndmy@users.noreply.github.com>
2026-07-01 08:24:31 +0700

    Add panelChannelId and panelMessageId to TicketConfig

--- models/TicketConfig.js
+++ models/TicketConfig.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const TicketConfigSchema = new mongoose.Schema({
     configId: { type: String, default: 'VIBEBLOX_TICKET' },
     ticketCounter: { type: Number, default: 0 },
+    panelChannelId: { type: String, default: null }, // <--- TAMBAHAN BARU
+    panelMessageId: { type: String, default: null }, // <--- TAMBAHAN BARU
     buttonStates: {
         type: Map,
         of: Boolean,
